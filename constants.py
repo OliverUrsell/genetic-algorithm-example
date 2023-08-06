c84c91b4374b69b8a4c0d8ad18c549c0d2347b9f
OliverUrsell <ollieursell@hotmail.co.uk>
2023-08-06 23:15:28 +0100

    Evaluate the creatures and cull and reproduce them to improve the population over multiple generations

--- constants.py
+++ constants.py
@@ -1,6 +1,10 @@
 
-representation_length = 10  # The number of binary characters a creature can represent
-mutation_probability = 0  # The probability of a mutation occurring
+# TODO: Refactor some of these to become parameters of the creatures themselves
+representation_length = 20  # The number of binary characters a creature can represent
+mutation_probability = 0.3  # The probability of a mutation occurring
+culling_proportion = 0.5  # The number of creatures to kill each generation
+population_size = 100  # The total size of the population
+generations = 200  # The total number of generations to perform
 
 if __name__ == '__main__':
     print("This file should not be run as main, run main.py instead")
--- main.py
+++ main.py
@@ -1,12 +1,31 @@
+from combination_functions import choose_random
+from constants import culling_proportion, population_size, generations
 from creature import Creature
 from evaluation_functions import distance_from_number
 
 
 def convert_number(num: int):
     # Create a randomly represented creature and print its representation
-    # This creature represents 172
-    c1 = Creature([False, False, True, False, True, False, True, True, False, False])
-    print(distance_from_number(c1, num))
+    population = [Creature() for _ in range(population_size)]
+
+    print(f"Trying to create a creature to match the number {str(bin(num))[2:]} - {num}\n")
+
+    for generation_number in range(generations):
+        print(f"Generation {generation_number}")
+
+        # Sort the population based on an evaluation function
+        population.sort(key=lambda c: distance_from_number(c, num))
+
+        print(f"Best creature: {population[0]} - {int(str(population[0]), 2)}")
+        print(f"Worst creature: {population[-1]} - {int(str(population[-1]), 2)}")
+
+        # Cull a proportion of the population
+        population = population[:int(len(population)*culling_proportion)]
+
+        # Combine the creatures according to a combination function to create the population for the next generation
+        population = choose_random(population)
+
+        print("")
 
 
 if __name__ == '__main__':
